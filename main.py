1f89a8271ee0db10301b6fb5d02388b41e32059c
ujju3073 <usharma1995@gmail.com>
2018-03-21 00:37:59 +0530

    my first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print ("Hello !")
+
+spy = raw_input("what is your spy name:? ")
+salutation = raw_input("What should we call you Mr or Ms ?")
+spyname = salutation +" "+spy
+print ("Alright "+ spyname +" I'd like to know little bit more about you")
